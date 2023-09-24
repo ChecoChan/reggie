a7ae97ccc214bf8a28dfe6217a1f5c3693d0c87e
Checo Chan <xzchan168@gmail.com>
2023-09-25 01:19:49 +0800

    update R.java

--- src/main/java/com/hcc/reggie/common/R.java
+++ src/main/java/com/hcc/reggie/common/R.java
@@ -10,14 +10,10 @@ import java.util.Map;
 /** 返回类 */
 @Data
 public class R<T> implements Serializable {
-
-    private Integer code; //编码：1成功，0和其它数字为失败
-
-    private String msg; //错误信息
-
-    private T data; //数据
-
-    private Map map = new HashMap(); //动态数据
+    private Integer code; // 编码：1 成功，0 或其它数字为失败
+    private String msg; // 错误信息
+    private T data; // 数据
+    private Map<String, Object> map = new HashMap<>(); // 动态数据
 
     public static <T> R<T> success(T object) {
         R<T> r = new R<T>();
@@ -27,7 +23,7 @@ public class R<T> implements Serializable {
     }
 
     public static <T> R<T> error(String msg) {
-        R r = new R();
+        R<T> r = new R<>();
         r.msg = msg;
         r.code = 0;
         return r;
@@ -37,5 +33,4 @@ public class R<T> implements Serializable {
         this.map.put(key, value);
         return this;
     }
-
 }
