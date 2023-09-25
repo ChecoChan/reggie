103ac2b7c857698eecf1ef8696912c8846a5d800
Checo Chan <xzchan168@gmail.com>
2023-09-25 17:58:55 +0800

    update SMSUtils.java

--- src/main/java/com/hcc/reggie/utils/SMSUtils.java
+++ src/main/java/com/hcc/reggie/utils/SMSUtils.java
@@ -7,21 +7,17 @@ import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.profile.DefaultProfile;
 
-/**
- * 短信发送工具类
- */
+/** 短信发送工具类 */
 public class SMSUtils {
-
     /**
      * 发送短信
-     *
      * @param signName     签名
      * @param templateCode 模板
      * @param phoneNumbers 手机号
      * @param param        参数
      */
     public static void sendMessage(String signName, String templateCode, String phoneNumbers, String param) {
-        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
+        DefaultProfile profile = DefaultProfile.getProfile("cn-guangzhou", "", "");
         IAcsClient client = new DefaultAcsClient(profile);
 
         SendSmsRequest request = new SendSmsRequest();
@@ -37,5 +33,4 @@ public class SMSUtils {
             e.printStackTrace();
         }
     }
-
 }
