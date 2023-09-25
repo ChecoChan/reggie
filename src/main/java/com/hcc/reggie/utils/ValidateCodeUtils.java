988ea9f80810b95592e67802abc488cda2cf8beb
Checo Chan <xzchan168@gmail.com>
2023-09-25 18:14:05 +0800

    update ValidateCodeUtils.java

--- src/main/java/com/hcc/reggie/utils/ValidateCodeUtils.java
+++ src/main/java/com/hcc/reggie/utils/ValidateCodeUtils.java
@@ -2,44 +2,25 @@ package com.hcc.reggie.utils;
 
 import java.util.Random;
 
-/**
- * 随机生成验证码工具类
- */
+/** 随机生成验证码工具类 */
 public class ValidateCodeUtils {
     /**
      * 随机生成验证码
-     *
-     * @param length 长度为4位或者6位
-     * @return
+     * @param length 生成验证码的长度
      */
     public static Integer generateValidateCode(int length) {
         Integer code = null;
         if (length == 4) {
-            code = new Random().nextInt(9999);//生成随机数，最大为9999
-            if (code < 1000) {
-                code = code + 1000;//保证随机数为4位数字
-            }
+            code = new Random().nextInt(9999); //生成随机数，最大为 9999
+            if (code < 1000)
+                code = code + 1000; // 保证随机数为 4 位数字
         } else if (length == 6) {
-            code = new Random().nextInt(999999);//生成随机数，最大为999999
-            if (code < 100000) {
-                code = code + 100000;//保证随机数为6位数字
-            }
+            code = new Random().nextInt(999999); //生成随机数，最大为 999999
+            if (code < 100000)
+                code = code + 100000; // 保证随机数为 6 位数字
         } else {
-            throw new RuntimeException("只能生成4位或6位数字验证码");
+            throw new RuntimeException("只能生成 4 位或 6 位数字验证码");
         }
         return code;
-    }
-
-    /**
-     * 随机生成指定长度字符串验证码
-     *
-     * @param length 长度
-     * @return
-     */
-    public static String generateValidateCode4String(int length) {
-        Random rdm = new Random();
-        String hash1 = Integer.toHexString(rdm.nextInt());
-        String capstr = hash1.substring(0, length);
-        return capstr;
     }
 }
